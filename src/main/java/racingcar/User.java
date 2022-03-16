66d7a2d8444ab7aa59019538e0f8b718d0366571
eunbii0213 <eunbii0213@naver.com>
2022-03-16 20:17:45 +0900

    feat : feature2

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -6,16 +6,19 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class User {
-    private final String USER_CAR_INPUT_GUIDE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
-    private final String USER_GAME_NUMBER_INPUT_GUIDE = "시도할 회수는 몇회인가요?";
-    private ArrayList<String> carName;
-    public StringTokenizer st;
+    private static final String USER_CAR_INPUT_GUIDE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+    private static final String USER_GAME_NUMBER_INPUT_GUIDE = "시도할 회수는 몇회인가요?";
+    private static ArrayList<String> carName;
+    public static StringTokenizer st;
+    public static int gameNumber;
+
 
     public User(){
         carName = new ArrayList<>();
     }
 
     public void userCarNameInput(){
+        System.out.println(USER_CAR_INPUT_GUIDE);
         st = new StringTokenizer(Console.readLine(),",");
     }
 
@@ -23,6 +26,11 @@ public class User {
         while(st.hasMoreTokens()){
             carName.add(st.nextToken());
         }
+    }
+
+    public void userGameNumberInput(){
+        System.out.println(USER_GAME_NUMBER_INPUT_GUIDE);
+        gameNumber = Integer.parseInt(Console.readLine());
     }
 
 }
